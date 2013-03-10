db0fcd2b84140ba2654735bcd9344422eb6178bb
mcupak <mcupak@redhat.com>
2013-03-09 23:32:28 -0500

    webapp cleanup

--- server/target/urbaneyes-server/WEB-INF/classes/import.sql
+++
@@ -1,2 +0,0 @@
--- You can use this file to load seed data into the database using SQL statements
-insert into Member (id, name, email, password) values (0, 'John Smith', 'foo@example.com', 'hello') 
