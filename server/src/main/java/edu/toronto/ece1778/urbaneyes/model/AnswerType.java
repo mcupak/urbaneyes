5507316025d43e2dd2b911720b1b3e82fa99317c
mcupak <mcupak@redhat.com>
2013-04-02 01:29:22 -0400

    removed noise as an answer type

--- server/src/main/java/edu/toronto/ece1778/urbaneyes/model/AnswerType.java
+++ server/src/main/java/edu/toronto/ece1778/urbaneyes/model/AnswerType.java
@@ -6,5 +6,5 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public enum AnswerType implements Serializable {
-	NUMBER, TEXT, YESNO, COUNT, NOISE;
+	NUMBER, TEXT, YESNO, COUNT;
 }
