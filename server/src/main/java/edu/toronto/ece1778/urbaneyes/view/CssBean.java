51d8252c702a4e70feb1030e2884f7215fc63730
mcupak <mcupak@redhat.com>
2013-04-02 00:59:37 -0400

    added tabs to the layout

---
+++ server/src/main/java/edu/toronto/ece1778/urbaneyes/view/CssBean.java
@@ -0,0 +1,31 @@
+package edu.toronto.ece1778.urbaneyes.view;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class CssBean implements Serializable {
+	private static final long serialVersionUID = -5156711102367948040L;
+
+	private long tab = 0;
+
+	public long getTab() {
+		return tab;
+	}
+
+	public void setTab(Long tab) {
+		this.tab = tab;
+	}
+
+	public void loadTab(Long t) {
+		setTab(t);
+	}
+
+	public boolean checkTab(Long index) {
+		return tab == index;
+	}
+
+}
